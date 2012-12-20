a25095a7ffaeedbcae21cf03b114c8983466ea93
Andrew Evans <andrew@atevans.com>
2012-12-20 08:22:13 -0800

    Fix time ordering, etc

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -7,4 +7,11 @@ class Follow < ActiveRecord::Base
   belongs_to :followed_user, class_name: "User"
   belongs_to :user
   
+  validate :not_following_self
+  validates_presence_of(:followed_user_id, :user_id)
+  
+  def not_following_self
+    errors.add(:base, "You can't follow yourself! This is a family game.") if followed_user_id == user_id
+  end
+  
 end
