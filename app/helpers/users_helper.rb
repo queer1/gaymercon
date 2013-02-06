6d2631c6afbec42e37364f3336c3adc072174e02
Andrew Evans <andrew@atevans.com>
2013-02-06 13:47:48 -0800

    Add map link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def common_games(user, other_user)
-    (user.game_groups & other_user.game_groups).collect{|g| link_to(g.name, group_path(g))}.join(", ")
+    (user.game_groups & other_user.game_groups).collect{|g| link_to(g.name, group_path(g))}.join(", ").html_safe
   end
   
   def common_networks(user, other_user)
-    (other_user.network_names & user.network_names).collect(&:humanize).join(", ")
+    (other_user.network_names & user.network_names).collect(&:humanize).join(", ").html_safe
   end
 end
